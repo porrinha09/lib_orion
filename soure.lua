c4b9eecdd33d96bb008c202c20caefc4277fb631
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2023-10-21 19:07:25 -0300

    Update soure.lua

--- soure.lua
+++ soure.lua
@@ -857,6 +857,7 @@ function OrionLib:MakeWindow(WindowConfig)
 				ButtonConfig = ButtonConfig or {}
 				ButtonConfig.Name = ButtonConfig.Name or "Button"
 				ButtonConfig.Callback = ButtonConfig.Callback or function() end
+				ButtonConfig.Icon = ButtonConfig.Icon or "rbxassetid://3944703587"
 
 				local Button = {}
 
